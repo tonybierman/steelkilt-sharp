0154d39db6f6652a6459d7b8fe798092dba19bf5
tonybierman <tonybierman@gmail.com>
2025-11-06 14:16:06 -0600

    Working on interactive combat sim

---
+++ Steelkilt.Examples.CliSim/ConsolePrompt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steelkilt.Examples.CliSim
+{
+    public static class ConsolePrompt
+    {
+        public static T GetUserChoice<T>(
+            string promptPrefix,
+            Dictionary<char, T> options,
+            T defaultValue) where T : notnull
+        {
+            var promptSuffix = GeneratePromptSuffix(options, defaultValue);
+            var fullPrompt = $"{promptPrefix} {promptSuffix}: ";
+
+            while (true)
+            {
+                Console.Write(fullPrompt);
+                var input = Console.ReadLine()?.ToUpper() ?? string.Empty;
+
+                if (string.IsNullOrWhiteSpace(input))
+                    return defaultValue;
+
+                if (options.TryGetValue(input[0], out var value))
+                    return value;
+
+                Console.WriteLine("Invalid option. Please try again.");
+            }
+        }
+
+        private static string GeneratePromptSuffix<T>(
+            Dictionary<char, T> options,
+            T defaultValue) where T : notnull
+        {
+            var parts = options.Select(kvp =>
+            {
+                var key = kvp.Key;
+                var value = kvp.Value.ToString() ?? "";
+                var isDefault = kvp.Value.Equals(defaultValue);
+
+                return $"[{key}]{value}";
+
+                //if (isDefault)
+                //    return $"[{key}]{value}";
+                //else
+                //    return $"{key}{value}";
+            });
+
+            var defaultValueStr = defaultValue.ToString() ?? "";
+            return "Select " + string.Join(", ", parts) + $" (default: {defaultValueStr})";
+        }
+    }
+}
---
+++ Steelkilt.Examples.CliSim/Program.cs
@@ -0,0 +1,95 @@
+﻿using SteelkiltSharp.Core;
+using SteelkiltSharp.Modules;
+
+namespace Steelkilt.Examples.CliSim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hero = new Character(
+                "Hero",
+                new Attributes(7, 8, 6, 6, 5, 6, 7, 6, 6),
+                weaponSkill: 6,
+                dodgeSkill: 6,
+                Weapon.LongSword(),
+                Armor.Leather());
+
+            var goblin = new Character(
+                "Goblin",
+                new Attributes(4, 7, 4, 5, 5, 4, 3, 6, 3),
+                weaponSkill: 4,
+                dodgeSkill: 5,
+                Weapon.Dagger(),
+                Armor.None());
+
+            var ManeuverTypeOptions = new Dictionary<char, ManeuverType>
+            {
+                { 'N', ManeuverType.Normal },
+                { 'C', ManeuverType.Charge },
+                { 'A', ManeuverType.AllOutAttack },
+                { 'D', ManeuverType.DefensivePosition }
+            };
+
+            var DefenseActionOptions = new Dictionary<char, DefenseAction>
+            {
+                { 'P', DefenseAction.Parry },
+                { 'D', DefenseAction.Dodge }
+            };
+
+            int round = 1;
+            while (!hero.IsDead && !goblin.IsDead)
+            {
+                Console.WriteLine($"Round {round}:");
+
+                var result = ConsolePrompt.GetUserChoice(
+                    promptPrefix: $"{hero.Name} stance?",
+                    options: ManeuverTypeOptions,
+                    defaultValue: ManeuverType.Normal
+                );
+
+                /* 
+                    Hero attacks
+                */
+
+                var heroAttackResult = Combat.CombatRound(hero, goblin, DefenseAction.Dodge);
+                Console.WriteLine($"  {heroAttackResult}");
+
+                if (goblin.IsDead)
+                {
+                    Console.WriteLine($"  {goblin.Name} has been defeated!");
+                    break;
+                }
+
+                /* 
+                    Goblin counterattacks
+                */
+
+                // Prompt for defense action
+                var defenseActionResult = ConsolePrompt.GetUserChoice(
+                    promptPrefix: $"{hero.Name} defense?",
+                    options: DefenseActionOptions,
+                    defaultValue: DefenseAction.Dodge
+                );
+
+                var goblinAttackResult = Combat.CombatRound(goblin, hero, defenseActionResult);
+                Console.WriteLine($"  {goblinAttackResult}");
+
+                if (hero.IsDead)
+                {
+                    Console.WriteLine($"  {hero.Name} has been defeated!");
+                    break;
+                }
+
+                Console.WriteLine($"  {hero.Name}: {hero.Wounds} | {goblin.Name}: {goblin.Wounds}");
+                round++;
+
+                if (round > 20)
+                {
+                    Console.WriteLine("  Combat lasted more than 20 rounds!");
+                    break;
+                }
+            }
+        }
+    }
+}
